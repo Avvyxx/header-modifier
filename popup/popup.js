068e72bc915e141f80b18e53495dd764ea798211
Avvyxx <luizgurrola@avyx.dev>
2024-08-30 16:57:22 -0700

    options page tests

--- popup/popup.js
+++ popup/popup.js
@@ -3,3 +3,7 @@ const body = document.getElementById('body');
 body.addEventListener('click', e => {
   console.log('hello world')
 })
+
+document.getElementById('test').addEventListener('click', () => {
+  browser.runtime.openOptionsPage();
+})
