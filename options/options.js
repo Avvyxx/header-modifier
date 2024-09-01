7981a5e7117a2b0a2fe7aefba8c36a5c4d782849
Luiz Gurrola <luizgurrola@avyx.dev>
2024-09-01 16:31:32 -0700

    added remove item functionality

--- options/options.js
+++ options/options.js
@@ -6,7 +6,12 @@ function add_item() {
   temp_node.removeAttribute('hidden');
   temp_node.removeAttribute('id');
 
+  temp_node.children.item(0).onclick = remove_item;
+
   container.appendChild(temp_node);
 }
 
+function remove_item(e) {
+  e.target.parentElement.remove();
+}
 document.getElementById('add-item').addEventListener('click', add_item);
